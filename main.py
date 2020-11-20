fd8472167966199243d295d4d11715fa10d24990
Alex <alexandre.pana@gmail.com>
2020-11-19 19:46:23 -0500

    Added some base files

---
+++ main.py
@@ -0,0 +1,21 @@
+#!python3
+
+
+episodes = 1000
+
+
+for episode in range(episodes):
+    # Initialize State
+
+    # Choose Initial Action from State using Policy
+
+    while (not current_state.termial):
+        # Execute Action, get Reward and State'
+
+        # Choose Action' from State' using Policy
+
+        # Update Policy
+
+        # S = S', A = A'
+        
+
--- sarsa.py
+++ sarsa.py
@@ -1,5 +1,27 @@
 #!python3
+import random
+
+class Sarsa:
+    def __init__(self, states, actions):
+        self.epsilon = 0.3
+        self.actions = actions
+        self.policy = {}
+        for state in states:
+            for action in actions:
+                self.policy[state][action] = 0
+
+    def update_policy(self, current_state, current_action, next_state, next_action, reward):
+        self.policy[current_state][current_action] = None
+    
+    def select_action(self, state):
+        return self.episilon_greedy(self.policy[state])
+    
+    def episilon_greedy(self, actions):
+        x = random.random()
+
+        if (x > self.epsilon):
+            return max(actions)
+        else:
+            return random.choice(actions)
 
 
-
-print(sarsa(grid))
