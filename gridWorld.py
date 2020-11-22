b453c9ca25d324036a14fbd6561694384d8450cd
Bryan Chafee <bryan.chafee@gmail.com>
2020-11-22 01:12:52 -0500

    Updated commenting for new implementations

--- gridWorld.py
+++ gridWorld.py
@@ -1,27 +1,31 @@
 import state
 import position
-
+#   ----Grid World----
+# This class object is the Grid World game
+# maintaining information on every grid tile
 class GridWorld:
-    def __init__(self, height, width, terminal_position, wind_columns1, wind_columns2): # terminal position is type Position, wind columns are lists of indexes with wind in them
-        self.height = height
+    def __init__(self, height, width, terminal_position, wind_columns1, wind_columns2): 
+        self.height = height                                                            
         self.width = width
-        self.wc1 = wind_columns1
-        self.wc2 = wind_columns2
-        self.terminal = terminal_position # Designate a terminal position, or target location
+        self.wc1 = wind_columns1 # wind columns are lists of [][indexes] with wind in them
+        self.wc2 = wind_columns2 # columns are either valued at 1 or 2
+        self.terminal = terminal_position # Designate a terminal position # terminal position is type Position,
         self.grid = self.generate_grid()
 
+    # used to by agents to check if a terminal state has been reached
     def is_position_terminal(self, position):
         return (position == self.terminal)
     
+    # grid world generator. This definition handles state creation, position assignment, and wind value assignment
     def generate_grid(self):
 
-        temp_grid = []
+        temp_grid = [] # temp list to be filled and returned as grid
 
-        for i in range(self.height):
+        for i in range(self.height): # Iterate over height of grid world
             
             temp = []
 
-            for j in range(self.width):
+            for j in range(self.width): # Iterate over width of grid world
 
                 new_state = state.State( position.Position(i,j) ) # Initialize new state
 
@@ -37,6 +41,6 @@ class GridWorld:
             
         return temp_grid
                 
-for ele in GridWorld(10, 10, position.Position(6,6), [1,3,5], [2,4,6]).grid:
-    for test in ele:
-        print(str(test.position.x) + " " + str(test.position.y) + " | Wind Value = " + str(test.wind))
+# for ele in GridWorld(10, 10, position.Position(6,6), [1,3,5], [2,4,6]).grid:
+#     for test in ele:
+#         print(str(test.position.x) + " " + str(test.position.y) + " | Wind Value = " + str(test.wind))
--- state.py
+++ state.py
@@ -1,7 +1,7 @@
 class State:
     def __init__(self, position):
-        self.position = position
-        self.wind = 0
+        self.position = position # Position is a vector2 point
+        self.wind = 0 # Wind effect on this grid state
     
-    def set_wind(self, value):
+    def set_wind(self, value): # Called to set a wind value to a grid state
         self.wind = value
