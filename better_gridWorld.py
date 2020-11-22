6a224ab1c20ce9d9733051d8ca9bdd2d926fef03
Alex <alexandre.pana@gmail.com>
2020-11-22 06:15:44 -0500

    Made my own grid object and reformatted main

---
+++ better_gridWorld.py
@@ -0,0 +1,68 @@
+from actions import Action
+import random
+#   ----Grid World----
+# This class object is the Grid World game
+# maintaining information on every grid tile
+class Grid:
+    def __init__(self, width, height, terminal_position, wind_columns1, wind_columns2):
+        self.width = width
+        self.height = height
+        self.terminal = terminal_position
+        self.generate_wind_array(wind_columns1, wind_columns2)
+        self.randomize_position()
+    
+    # Create 2D array of wind at each position
+    def generate_wind_array(self, wc1, wc2):
+        self.wind_array = [[0 for x in range(self.width)] for y in range(self.height)]
+        for r in range(self.height):
+            for c in range(self.width):
+                wind = 0
+                if (c in wc1):
+                    wind = 1
+                elif (c in wc2):
+                    wind = 2
+                
+                self.wind_array[r][c] = wind
+                
+
+    # Returns true if given position is terminal
+    def at_terminal(self):
+        return (self.x == self.terminal[0] and self.y == self.terminal[1])
+    
+    # Returns a random coordinate on the grid
+    def randomize_position(self):
+        self.x = random.randrange(0, self.width)
+        self.y = random.randrange(0, self.height)
+    
+    # Lets us print the grid in a nice format
+    def __str__(self):
+        string = f"\n"
+
+        for r in range(self.height):
+            for c in range(self.width):
+                string = f"{string} {self.wind_array[r][c]}"
+            string = f"{string}\n"
+        
+        return string
+    
+    def move(self, action):
+        if (action == Action.UP):
+            self.y += 1
+        elif (action == Action.DOWN):
+            self.y -= 1
+        elif (action == Action.LEFT):
+            self.x -= 1
+        elif (action == Action.RIGHT):
+            self.x += 1
+
+    # Bulkier String
+    # def __str__(self):
+    #     divider = ("----" * self.width) + "--"
+    #     string = f"\n{divider}\n"
+
+    #     for r in range(self.height):
+    #         for c in range(self.width):
+    #             string = f"{string} | {self.wind_array[r][c]}"
+    #         string = f"{string} |\n{divider}\n"
+        
+    #     return string
--- main.py
+++ main.py
@@ -1,21 +1,77 @@
 #!python3
+from actions import Action
+import gridWorld
+import better_gridWorld
+import position
+import sarsa
+import q_learning
+
+def train_sarsa(grid, episodes):
+    for episode in range(episodes):
+        # Initialize State
+
+        # Choose Initial Action from State using Policy
+
+        while (not current_state.termial):
+            pass
+            # Execute Action, get Reward and State'
+
+            # Choose Action' from State' using Policy
+
+            # Update Policy
+
+            # S = S', A = A'
+
+def train_q_learning(grid, episodes, actions):
+    q_learner = q_learning.Q_Learner(grid, actions)
+    print(grid)
+    print(f'Starting Position on Grid: ({grid.x}, {grid.y})')
+
+    # Loop for each episode
+    for episode in range(episodes):
+        # Initialize state
+        grid.randomize_position()
+
+        # Loop for each step of episode
+        while (not grid.at_terminal()):
+            # grid.randomize_position()
+
+            print(f'Before action: ({grid.x}, {grid.y})')
+
+            # Choose A from S using policy derived from Q
+            action = q_learner.select_action(grid)
+
+            # Execute action
+            grid.move(action)
+
+            print(f'After {action}: ({grid.x}, {grid.y})')
+
+            input("Press Enter to continue...")
 
 
-episodes = 1000
 
 
-for episode in range(episodes):
-    # Initialize State
 
-    # Choose Initial Action from State using Policy
+def __main__():
+    # Define training variables
+    episodes = 1000
+    cardinal_moves = [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT]
+    kings_moves = [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.UPRIGHT, Action.UPLEFT, Action.DOWNRIGHT, Action.DOWNLEFT]
 
-    while (not current_state.termial):
-        # Execute Action, get Reward and State'
+    # Define our Grid
+    width = 10
+    height = 7
+    wind_columns1 = [3, 4, 5, 8]
+    wind_columns2 = [6, 7]
+    terminal_position = (7, 4)
+    grid = better_gridWorld.Grid(width, height, terminal_position, wind_columns1, wind_columns2)
 
-        # Choose Action' from State' using Policy
+    # train_sarsa(episodes)
 
-        # Update Policy
+    train_q_learning(grid, episodes, cardinal_moves)
 
-        # S = S', A = A'
+
+    
         
-
+if __name__ == '__main__':
+    __main__()
