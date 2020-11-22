847253281102acd5745a60505f3ffd62fe966f5c
Bryan Chafee <bryan.chafee@gmail.com>
2020-11-22 17:31:30 -0500

    removed deprecated files

--- position.py
+++
@@ -1,5 +0,0 @@
-# Simple class responsible for holding a Vector2 [x,y] position in Grid World, used namely for readibility
-class Position: 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
